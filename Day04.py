57172a3f67f7b17c617aa7294bee2d13d405c83a
Michelle <clairsin@gmail.com>
2022-12-05 13:35:35 -0500

    completed part 2

--- Day04.py
+++ Day04.py
@@ -1,7 +1,8 @@
 with open('Day04data.in') as data:
   pairs = [i for i in data.read().strip().split('\n')]
 
-count = 0
+fully_overlap_count = 0
+partial_overlap_count = len(pairs)
 
 for pair in pairs:
   p1, p2 = pair.split(',')
@@ -10,8 +11,11 @@ for pair in pairs:
   print(p2_start, p2_end, p1_start, p1_end)
 
   if (p1_start <= p2_start and p1_end >= p2_end) or (p1_start >= p2_start and p1_end <= p2_end):
-    count +=1
+    fully_overlap_count +=1
+  
+  if (p2_start > p1_end) or (p1_start > p2_end):
+    partial_overlap_count -=1
+  
     
-print("Part 1: ", count)
-
-# Some of the pairs have noticed that one of their assignments fully contains the other. For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6. In pairs where one assignment fully contains the other, one Elf in the pair would be exclusively cleaning sections their partner will already be cleaning, so these seem like the most in need of reconsideration. In this example, there are 2 such pairs.
+print("Part 1: ", fully_overlap_count)
+print("Part 2: ", partial_overlap_count)
