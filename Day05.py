1caa61a8f548637f71cd7dfba6390322a431ffe8
Michelle <clairsin@gmail.com>
2022-12-05 16:24:53 -0500

    transformed dictionary

--- Day05.py
+++ Day05.py
@@ -1,9 +1,33 @@
 with open('Day05data.in') as data:
   stacks = [i for i in data.read().split('\n')]
 
-print(stacks[8])
+# print(stacks[8])
 transformed_dict = {}
 
-for key in len(stacks):
-  transformed_dict.append(stacks[8])
+for char in stacks[8]:
+  if char != " ":
+    transformed_dict[int(char)] = []
 
+for stack in reversed(stacks[0:8]):
+  key = 1
+  pos = 1
+  print(transformed_dict[key])
+  while (key < 10):
+    if (stack[pos] != "[") and (stack[pos] != "]") and (stack[pos] != " "):
+      transformed_dict[key] += [stack[pos]]
+    key +=1
+    pos +=4
+
+
+print(transformed_dict)
+
+
+#             [G] [W]         [Q]    
+# [Z]         [Q] [M]     [J] [F]    
+# [V]         [V] [S] [F] [N] [R]    
+# [T]         [F] [C] [H] [F] [W] [P]
+# [B] [L]     [L] [J] [C] [V] [D] [V]
+# [J] [V] [F] [N] [T] [T] [C] [Z] [W]
+# [G] [R] [Q] [H] [Q] [W] [Z] [G] [B]
+# [R] [J] [S] [Z] [R] [S] [D] [L] [J]
+#  1   2   3   4   5   6   7   8   9 
