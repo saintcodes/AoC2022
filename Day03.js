9021e31e6c5a1856475d3856999ea1e20c0dd799
Michelle <clairsin@gmail.com>
2022-12-05 13:22:30 -0500

    completed part1

--- Day03.js
+++ Day03.js
@@ -4,4 +4,5 @@ fs.readFile('Day03data.txt', (err, data) => {
     if (err) throw err;
     let stringData = data.toString();
     console.log(stringData, "a;lskdfj")
+    
   })
--- Day04.py
+++ Day04.py
@@ -1,43 +1,17 @@
 with open('Day04data.in') as data:
   pairs = [i for i in data.read().strip().split('\n')]
 
-
-# print(pairs)
+count = 0
 
 for pair in pairs:
-  splitPairs = pair.split(',')
-  a = int(splitPairs[0][0])
-  print(a)
-  # if int(splitPairs[0]) in int(splitPairs[1]):
-  #   print('hello')
-# 2-4,6-8
-# 2-3,4-5
-# 5-7,7-9
-# 2-8,3-7
-# 6-6,4-6
-# 2-6,4-8
-# For the first few pairs, this list means:
+  p1, p2 = pair.split(',')
+  p1_start, p1_end = map(int, p1.split('-'))
+  p2_start, p2_end = map(int, p2.split('-'))
+  print(p2_start, p2_end, p1_start, p1_end)
 
-# Within the first pair of Elves, the first Elf was assigned sections 2-4 (sections 2, 3, and 4), while the second Elf was assigned sections 6-8 (sections 6, 7, 8).
-# The Elves in the second pair were each assigned two sections.
-# The Elves in the third pair were each assigned three sections: one got sections 5, 6, and 7, while the other also got 7, plus 8 and 9.
-# This example list uses single-digit section IDs to make it easier to draw; your actual list might contain larger numbers. Visually, these pairs of section assignments look like this:
+  if (p1_start <= p2_start and p1_end >= p2_end) or (p1_start >= p2_start and p1_end <= p2_end):
+    count +=1
+    
+print("Part 1: ", count)
 
-# .234.....  2-4
-# .....678.  6-8
-
-# .23......  2-3
-# ...45....  4-5
-
-# ....567..  5-7
-# ......789  7-9
-
-# .2345678.  2-8
-# ..34567..  3-7
-
-# .....6...  6-6
-# ...456...  4-6
-
-# .23456...  2-6
-# ...45678.  4-8
 # Some of the pairs have noticed that one of their assignments fully contains the other. For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6. In pairs where one assignment fully contains the other, one Elf in the pair would be exclusively cleaning sections their partner will already be cleaning, so these seem like the most in need of reconsideration. In this example, there are 2 such pairs.
